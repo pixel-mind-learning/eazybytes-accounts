a7c91bae735c4173b64507ffc30600841de1ba3f
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-06-14 06:34:37 +0530

    changes

---
+++ src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -0,0 +1,20 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Record class to hold contact information for accounts. As a data carrier
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "accounts")
+public class AccountsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
